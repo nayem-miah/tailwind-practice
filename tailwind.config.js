545ea6efa90c9b6c17844ff13f00a84744258051
Nayem Islam <53568454+nayem-miah@users.noreply.github.com>
2025-01-04 03:13:17 +0600

    'commited'

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust this path as per your project structure
+    './index1.html', // Include any other relevant files
+    './index.html', // Include any other relevant files
+    './index2.html', // Include any other relevant files
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary":"#0092E4",
+        "secondary":"#183656"
+      }
+    },
+  },
+  plugins: [],
+};
+
+
+
+
